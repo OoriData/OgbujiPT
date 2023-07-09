8c942f97d99d0466d0049c15245cb767d6fbf059
Uche Ogbuji <uche@ogbuji.net>
2023-07-09 17:30:43 -0600

    Implement mocking for test_embedding_helper.py. Still needs work.

--- test/test_embedding_helper.py
+++ test/test_embedding_helper.py
@@ -9,19 +9,15 @@ Uses the COME_THUNDER_POEM fixture from conftest.py
 '''
 # import pytest
 
-# from qdrant_client.http.models.models import ScoredPoint
-
-from sentence_transformers import SentenceTransformer
-
-from ogbujipt.embedding_helper import initialize_embedding_db, upsert_embedding_db
+from ogbujipt import embedding_helper
+from ogbujipt.embedding_helper import qdrant_init_embedding_db, \
+    qdrant_upsert_embedding_db
 from ogbujipt.text_helper import text_splitter
 
 
-def test_embed_poem(COME_THUNDER_POEM):
+def test_embed_poem(mocker, COME_THUNDER_POEM):
     # LLM will be downloaded from HuggingFace automatically
     # FIXME: We want to mock this instead
-    embedding = SentenceTransformer(DOC_EMBEDDINGS_LLM)
-
     # Split the chunks
     chunks = text_splitter(
         COME_THUNDER_POEM, 
@@ -32,34 +28,37 @@ def test_embed_poem(COME_THUNDER_POEM):
 
     collection_name = 'test_collection'
 
-    # initialize a client
-    client = initialize_embedding_db(
+    # TODO: Add more shape to the mocking, to increase the tests's usefulness
+    embedding = mocker.MagicMock()
+    embedding_helper.models = mocker.MagicMock()
+    mock_vparam = object()
+    embedding_helper.models.VectorParams.side_effect = [mock_vparam]
+    mocker.patch('ogbujipt.embedding_helper.QdrantClient')
+
+    client = qdrant_init_embedding_db(
         chunks,
         embedding,
         collection_name
         )
+    client.recreate_collection.assert_called_once_with(
+        collection_name='test_collection',
+        vectors_config=mock_vparam
+        )
+    embedding.encode.assert_called_once_with(list(chunks[0]))
 
-    # insert the chunks into the Qdrant client
-    client = upsert_embedding_db(
+    # Test update/insert into the DB
+    mock_pstruct = object()
+    embedding_helper.models.PointStruct.side_effect = lambda id=None, vector=None, payload=None: mock_pstruct
+    client.count.side_effect = ['count=0']
+
+    client = qdrant_upsert_embedding_db(
         client, 
         chunks, 
         embedding, 
         collection_name
         )
 
-    embedded_question = embedding.encode(TEST_QUESTION)
-
-    relevant_answers = client.search(
-        collection_name=collection_name, 
-        query_vector=embedded_question, 
-        limit=1
+    client.upsert.assert_called_with(
+        collection_name=collection_name,
+        points=[mock_pstruct]
         )
-
-    joined_answers = ''.join(relevant_answers[0].payload['chunk_string'])
-
-    assert 'O dancers' in joined_answers
-
-
-DOC_EMBEDDINGS_LLM = 'all-MiniLM-L6-v2'
-
-TEST_QUESTION = 'Hey dancers'
