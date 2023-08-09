e0ba05cb869161ca4b3c17637f5167b039d7103e
choccccy <osita@ogbuji.net>
2023-08-09 15:26:01 -0600

    fix testing for embedding_helper.py

--- test/test_embedding_helper.py
+++ test/test_embedding_helper.py
@@ -21,6 +21,34 @@ def CORRECT_STRING():
     return 'And the secret thing in its heaving\nThreatens with iron mask\nThe last lighted torch of the century…'
 
 
+class SentenceTransformer:
+    """
+    Fake class for testing
+    """
+    def __init__(self, model_name_or_path = None,
+                 modules= None,
+                 device = None,
+                 cache_folder = None,
+                 use_auth_token = None
+                 ):
+        pass
+
+
+    def encode(self, sentences,
+               batch_size: int = 32,
+               show_progress_bar= None,
+               output_value: str = 'sentence_embedding',
+               convert_to_numpy: bool = True,
+               convert_to_tensor: bool = False,
+               device= None,
+               normalize_embeddings: bool = False):
+        pass
+        """
+        Property to set the maximal input sequence length for the model. Longer inputs will be truncated.
+        """
+        self._first_module().max_seq_length = value
+
+
 def test_embed_poem(mocker, COME_THUNDER_POEM, CORRECT_STRING):
     # LLM will be downloaded from HuggingFace automatically
     # FIXME: We want to mock this instead, or rather just have a fixture with the results
@@ -35,13 +63,13 @@ def test_embed_poem(mocker, COME_THUNDER_POEM, CORRECT_STRING):
     collection_name = 'test_collection'
 
     # TODO: Add more shape to the mocking, to increase the tests's usefulness
-    embedding = mocker.MagicMock()
+    embedding_model = mocker.MagicMock(spec=SentenceTransformer)
     embedding_helper.models = mocker.MagicMock()
     mock_vparam = object()
     embedding_helper.models.VectorParams.side_effect = [mock_vparam]
     mocker.patch('ogbujipt.embedding_helper.QdrantClient')
 
-    coll = qdrant_collection(collection_name, embedding)
+    coll = qdrant_collection(name=collection_name, embedding_model=embedding_model)
 
     # client.count.side_effect = ['count=0']
     coll.db.count.side_effect = lambda collection_name: 'count=0'
@@ -51,7 +79,7 @@ def test_embed_poem(mocker, COME_THUNDER_POEM, CORRECT_STRING):
         vectors_config=mock_vparam
         )
 
-    embedding.encode.assert_called_with(CORRECT_STRING)
+    embedding_model.encode.assert_called_with(CORRECT_STRING)
 
     # Test update/insert into the DB
     mock_pstruct = object()
