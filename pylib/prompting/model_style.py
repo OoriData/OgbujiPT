79d2f5c8fa7ecf328b08dc32370a2e3e360bab5d
Uche Ogbuji <uche@ogbuji.net>
2023-08-15 23:41:00 -0600

    [#10] Shotgun forward changes for model introspection support

--- pylib/prompting/model_style.py
+++ pylib/prompting/model_style.py
@@ -6,9 +6,43 @@
 Delimiters for common LLM model prompting styles.
 
 For details see https://github.com/uogbuji/OgbujiPT/wiki/Prompt-templates
+
+Also includes model introspection, e.g.:
+
+>>> from ogbujipt.prompting.model_style import hosted_model_openai
+>>> from ogbujipt.config import openai_emulation
+>>> openai_emulation(host='http://127.0.0.1', port='8000')
+>>> print(hosted_model_openai())
+['/models/TheBloke_WizardLM-13B-V1.0-Uncensored-GGML/wizardlm-13b-v1.0-uncensored.ggmlv3.q6_K.bin']
 '''
 
+import re
+from typing import List
+from enum import Enum
+
 from ogbujipt.prompting.basic import pdelim, ordering
+
+# __all__ = ['hosted_model_openai']
+
+# Quick cheat sheet:
+# Alpaca style = [A…] \n ### Response:
+# Alpaca/Instruct style = ### Instruction:\n [A…] ### Response:
+# Alpaca/Instruct style, with input = ### Instruction:\n [A…] ### Input:\n ### Response:
+# Vicuña style = ### Human:\n [A…] ### Assistant:
+# Wizard style = USER:\n [A…] ### ASSISTANT:
+
+
+class style(Enum):
+    '''
+    Marker of different prompring styles. When LLMs are trained, they're tuned
+    to expect prompts according to a specific convention, and can become very
+    erratic if you use the wrong one
+    '''
+    ALPACA = 1
+    ALPACA_INSTRUCT = 2
+    VICUNA = 3
+    WIZARD = 4
+
 
 VICUNA_DELIMITERS = {
     pdelim.PREQUERY: '### USER:',
@@ -80,6 +114,7 @@ OPENAI_GPT_DELIMITERS = {
 {{ user_message }} [/INST]
 '''
 
+# XXX: Rethink in light of word loom
 def concat_input_prompts(context_content_pairs):
     '''
     Turn a sequence of context / text pairs into a composite
@@ -94,3 +129,48 @@ def concat_input_prompts(context_content_pairs):
     # pre_tmpl = ''.join(parts)
     # return PromptTemplate.from_template(''.join(parts))
     return ''.join(parts)
+
+
+MODEL_NAME_SEARCH_PATTERNS = {
+    re.compile('nous-hermes'): [style.ALPACA_INSTRUCT],
+    re.compile('wizardlm'): [style.WIZARD],
+    # re.compile(''): [style.ALPACA_INSTRUCT],
+}
+
+
+def hosted_model_openai() -> List[str]:
+    '''
+    Query the OpenAI-compatible API set up via openai_emulation()
+    (or even the real deal)
+    to find what model is being run for APi calls
+    '''
+    try:
+        import httpx  # noqa
+    except ImportError:
+        raise RuntimeError('Needs httpx installed. Try pip install httpx')
+    import openai
+
+    resp = httpx.get(f'{openai.api_base}/models').json()
+    # print(resp)
+    model_fullname = [i['id'] for i in resp['data']]
+    return model_fullname
+
+
+def model_style_from_name(mname: str) -> List[str]:
+    '''
+    Uses heuristics to figure out the prompting/model style from its name
+
+    >>> from ogbujipt.model_style import model_style_from_name
+    >>> model_style_from_name('path/wizardlm-13b-v1.0-uncensored.ggmlv3.q6_K.bin')
+    [<style.WIZARD: 4>]
+    '''
+    # Use the final slash portion, in case it's a full path
+    mname = mname.split('/')[-1]
+    for pat, styles in MODEL_NAME_SEARCH_PATTERNS.items():
+        if pat.search(mname):
+            mstyles = styles
+            break
+    else:
+        # XXX: Come up with a default/unknown style?
+        mstyles = []
+    return mstyles
