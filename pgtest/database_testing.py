9d01bfd6fab28eba99a65814f0398ff88c71d9d5
choccccy <osita@ogbuji.net>
2023-08-31 13:46:57 -0600

    even more freshness

--- pgtest/database_testing.py
+++ pgtest/database_testing.py
@@ -3,7 +3,6 @@ import asyncio
 import os
 from sentence_transformers import SentenceTransformer
 from dotenv import load_dotenv
-import numpy
 
 load_dotenv()
 print('USER:', os.getenv('DB_USER'))
@@ -12,8 +11,8 @@ e_model = SentenceTransformer("all-mpnet-base-v2")
 
 lorem_ipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vestibulum nisl eget mauris malesuada, quis facilisis arcu vehicula. Sed consequat, quam ut auctor volutpat, augue ex tincidunt massa, in varius nulla ex vel ipsum. Nullam vitae eros nec ante sagittis luctus. Nullam scelerisque dolor eu orci iaculis, at convallis nulla luctus. Praesent eget ex id arcu facilisis varius vel id neque. Donec non orci eget elit aliquam tempus. Sed at tortor at tortor congue dictum. Nulla varius erat at libero lacinia, id dignissim risus auctor. Ut eu odio vehicula, tincidunt justo ac, viverra erat. Sed nec sem sit amet erat malesuada finibus. Nulla sit amet diam nec dolor tristique dignissim. Sed vehicula, justo nec posuere eleifend, libero ligula interdum neque, at lacinia arcu quam non est. Integer aliquet, erat id dictum euismod, felis libero blandit lorem, nec ullamcorper quam justo at elit.'
 
-e_lorem_ipsum = e_model.encode(lorem_ipsum, show_progress_bar=True)
-print(len(e_lorem_ipsum))
+e_lorem_ipsum = e_model.encode(lorem_ipsum)
+
 
 async def main():
     print('Connecting to database...')
@@ -28,11 +27,11 @@ async def main():
     print('Connected to database')
 
     print('Ensuring that the vector extension is installed...')
-    await vDB.raw_sql(f'''CREATE EXTENSION IF NOT EXISTS vector;''')
+    await vDB.raw_sql('''CREATE EXTENSION IF NOT EXISTS vector;''')
     print('Ensured that the vector extension is installed')
 
     print('Dropping old table...')
-    await vDB.raw_sql(f'''DROP TABLE IF EXISTS embeddings;''')
+    await vDB.raw_sql('''DROP TABLE IF EXISTS embeddings;''')
     print('Dropped old table')
 
     print('Creating new table...')
