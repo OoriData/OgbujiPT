53e5b263d9282a465c4b78d7659c8b3e33a6e11a
choccccy <osita@ogbuji.net>
2023-11-06 17:31:54 -0700

    first draft and demo notebook working

--- pylib/embedding/pgvector.py
+++ pylib/embedding/pgvector.py
@@ -47,9 +47,9 @@ INSERT INTO {table_name} (
 ) VALUES ($1, $2, $3, $4, $5, $6);
 '''
 
-SEARCH_DOC_TABLE = '''-- Semantic search a document
+QUERY_DOC_TABLE = '''-- Semantic search a document
 SELECT 
-    (embedding <=> '{search_embedding}') AS cosine_similarity,
+    (embedding <=> '{query_embedding}') AS cosine_similarity,
     title,
     content,
     permission,
@@ -78,12 +78,11 @@ CREATE TABLE IF NOT EXISTS {table_name} (
 INSERT_CHATLOG = '''-- Insert a message into a chatlog
 INSERT INTO {table_name} (
     history_key,
-    index,
     role,
     content,
     embedding,
     metadata_JSON
-) VALUES ($1, $2, $3, $4, $5, $6);
+) VALUES ($1, $2, $3, $4, $5);
 '''
 
 RETURN_CHATLOG_BY_HISTORY_KEY = '''-- Get entire chatlog of a history key
@@ -100,9 +99,9 @@ ORDER BY
     index ASC;
 '''
 
-SEMANTIC_SEARCH_CHATLOG_TABLE = '''-- Semantic search a chatlog
+SEMANTIC_QUERY_CHATLOG_TABLE = '''-- Semantic search a chatlog
 SELECT 
-    (embedding <=> '{search_embedding}') AS cosine_similarity,
+    (embedding <=> '{query_embedding}') AS cosine_similarity,
     index,
     role,
     content,
@@ -115,7 +114,6 @@ ORDER BY
     cosine_similarity ASC
 LIMIT {limit};
 '''
-
 # ======================================================================================================================
 
 class PGvectorHelper:
@@ -138,7 +136,6 @@ class PGvectorHelper:
         if not PREREQS_AVAILABLE:
             raise RuntimeError('pgvector not installed, you can run `pip install pgvector asyncpg`')
 
-        print(1, table_name)
         if not table_name.replace('_', '').isalnum():
             raise ValueError('table_name must be alphanumeric, with underscore also allowed')
 
@@ -156,9 +153,9 @@ class PGvectorHelper:
         self.table_name = table_name
 
     @classmethod
-    async def from_conn_params(cls, embedding_model, table_name,
-                                user, password, db_name, host, port, **conn_params
-            ) -> 'PGvectorHelper':
+    async def from_conn_params(cls, embedding_model, table_name, user, 
+                               password, db_name, host, port, **conn_params
+    ) -> 'PGvectorHelper':
         '''
         Create a PGvector helper from connection parameters
 
@@ -176,7 +173,7 @@ class PGvectorHelper:
             )
         except Exception as e:
             # Don't blanket mask the exception. Handle exceptions types in whatever way makes sense
-            raise
+            raise e
         return await cls.from_connection(embedding_model, table_name, conn)
 
     @classmethod
@@ -215,7 +212,7 @@ class PGvectorHelper:
             title: str = 'NULL',
             page_numbers: list[int] = [],
             tags: list[str] = []
-        ) -> None:
+    ) -> None:
         '''
         Update a table with one embedded document
 
@@ -232,8 +229,15 @@ class PGvectorHelper:
         '''
         # Get the embedding of the content as a PGvector compatible list
         content_embedding = self._embedding_model.encode(content)
-        await self.conn.execute(INSERT_DOCS.format(table_name=self.table_name), content_embedding.tolist(), content,
-                                permission, title, page_numbers, tags)
+        await self.conn.execute(
+            INSERT_DOCS.format(table_name=self.table_name),
+            content_embedding.tolist(),
+            content,
+            permission,
+            title,
+            page_numbers,
+            tags
+        )
 
     async def insert_docs(
             self,
@@ -247,16 +251,19 @@ class PGvectorHelper:
         Args:
             content_list (Sequence[ .. ]): A list of tuples, each of the form: (content, permission, title, page_numbers, tags)
         '''
-        await self.conn.execute(INSERT_DOCS.format(table_name=self.table_name), [
-            (self._embedding_model.encode(content), content, permission, title, page_numbers, tags)
-            for content, permission, title, page_numbers, tags in content_list
-        ])
+        await self.conn.execute(
+            INSERT_DOCS.format(table_name=self.table_name),
+            [
+                (self._embedding_model.encode(content), content, permission, title, page_numbers, tags)
+                for content, permission, title, page_numbers, tags in content_list
+            ]
+        )
 
     async def search_doc_table(
             self,
             query_string: str,
             limit: int = 1
-            ) -> list[asyncpg.Record]:
+    ) -> list[asyncpg.Record]:
         '''
         Similarity search documents using a query string
 
@@ -274,14 +281,127 @@ class PGvectorHelper:
         # Search the table
         # FIXME: Figure out the SQL injection guard for vectors. Not sure SQL Query params is an option here
         search_results = await self.conn.fetch(
-            SEARCH_DOC_TABLE.format(
+            QUERY_DOC_TABLE.format(
                 table_name=self.table_name,
-                search_embedding=query_embedding,
-                limit=limit)
+                query_embedding=query_embedding,
+                limit=limit
             )
+        )
+        return search_results
+    
+    async def create_chatlog_table(self):
+        '''
+        Create the table to hold chatlogs
+        '''
+        # Check that the connection is still alive
+        # if self.conn.is_closed():
+        #     raise ConnectionError('Connection to database is closed')
+
+        # Create the table
+        await self.conn.execute(
+            CREATE_CHATLOG_TABLE.format(
+                table_name=self.table_name,
+                embed_dimension=self._embed_dimension)
+            )
+
+    async def insert_message(
+            self,
+            history_key: str,
+            role: str,
+            content: str,
+            metadata: dict = {}
+    ) -> None:
+        '''
+        Update a table with one embedded document
+
+        Args:
+            history_key (str): history key (unique identifier) of the chatlog this message belongs to
+
+            role (str): role of the message (system, user, assistant, tool (formerly 'function'))
+
+            content (str): text content of the message
+
+            metadata (dict): additional metadata of the message
+        '''
+        match role:  # Set role as an integer
+            case 'system':
+                role_int = 0
+            case 'user':
+                role_int = 1
+            case 'assistant':
+                role_int = 2
+            case 'tool':  # formerly 'function'
+                role_int = 3
+            case _:
+                role_int = -1
+
+        # Get the embedding of the content as a PGvector compatible list
+        content_embedding = self._embedding_model.encode(content)
+
+        await self.conn.execute(
+            INSERT_CHATLOG.format(table_name=self.table_name),
+            history_key,
+            role_int,
+            content,
+            content_embedding.tolist(),
+            metadata
+        )
+    
+    async def get_chatlog(
+            self,
+            history_key: str
+    ) -> list[asyncpg.Record]:
+        '''
+        Get the entire chatlog of a history key
+
+        Args:
+            history_key (str): history key of the chatlog
+        Returns:
+            list[asyncpg.Record]: list of chatlog
+            asyncpg.Record objects are similar to dicts, but allow for attribute-style access
+        '''
+        # Get the chatlog
+        chatlog = await self.conn.fetch(
+            RETURN_CHATLOG_BY_HISTORY_KEY.format(
+                table_name=self.table_name,
+                history_key=history_key
+            )
+        )
+        return chatlog
+    
+    async def search_chatlog(
+            self,
+            history_key: str,
+            query_string: str,
+            limit: int = 1
+    ) -> list[asyncpg.Record]:
+        '''
+        Similarity search documents using a query string
+
+        Args:
+            query_string (str): string to compare against items in the table
+
+            k (int): maximum number of results to return (useful for top-k query)
+        Returns:
+            list[asyncpg.Record]: list of search results
+            asyncpg.Record objects are similar to dicts, but allow for attribute-style access
+        '''
+        # Get the embedding of the query string as a PGvector compatible list
+        query_embedding = list(self._embedding_model.encode(query_string))
+
+        # Search the table
+        # FIXME: Figure out the SQL injection guard for vectors. Not sure SQL Query params is an option here
+        search_results = await self.conn.fetch(
+            SEMANTIC_QUERY_CHATLOG_TABLE.format(
+                table_name=self.table_name,
+                query_embedding=query_embedding,
+                history_key=history_key,
+                limit=limit
+            )
+        )
         return search_results
 
-    async def count_docs(self) -> int:
+    async def count_entries(self) -> int:
         '''
         Count the number of documents in the table
 
@@ -293,7 +413,7 @@ class PGvectorHelper:
         count = await self.conn.fetchval(f'SELECT COUNT(*) FROM {self.table_name}')
         return count
 
-    async def drop_doc_table(self) -> None:
+    async def drop_table(self) -> None:
         '''
         Delete the table
 
