c72933cf7cc0a16db1e6e93b2ac761e15e68982f
choccccy <osita@ogbuji.net>
2024-06-14 15:51:46 -0600

    better assert for match fail

--- pylib/embedding/pgvector.py
+++ pylib/embedding/pgvector.py
@@ -199,7 +199,7 @@ def match_exact(key, val):
     '''
     Filter specifier to only return rows where the given top-level key exists in metadata, and matches the given value
     '''
-    assert key.replace('_', '').replace('-', '').isalnum(), 'key_'
+    assert key.replace('_', '').replace('-', '').isalnum(), 'key contains nonalphanumeric, "-", or "_" characters'
     if isinstance(val, str):
         cast = ''
     elif isinstance(val, bool):
@@ -220,7 +220,7 @@ def match_oneof(key, options: tuple[str]):
     '''
     options = tuple(options)
     assert options
-    assert key.replace('_', '').replace('-', '').isalnum(), 'key_'
+    assert key.replace('_', '').replace('-', '').isalnum(), 'key contains nonalphanumeric, "-", or "_" characters'
     option1 = options[0]
     if isinstance(option1, str):
         cast = ''
